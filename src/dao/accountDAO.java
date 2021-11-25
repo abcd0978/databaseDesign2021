adc7ba2a640e606be1124c42e32998eaa249b06e
kimmingyu <kimmin36944@gmail.com>
2021-11-25 20:44:22 +0900

    accountDAO 수정

--- src/dao/accountDAO.java
+++ src/dao/accountDAO.java
@@ -10,20 +10,20 @@ import dto.accountLogDTO;
 import java.util.ArrayList;
 
 public class accountDAO {
-	//계좌들조회
-	public ArrayList<accountDTO> selectAccounts(String user_id) throws SQLException, ClassNotFoundException
+	// 계좌들조회
+	public ArrayList<accountDTO> selectAccounts(String user_id) throws SQLException, ClassNotFoundException 
 	{
 		Connection con = DBConnection.getConnection();
 		PreparedStatement psmt;
 		ResultSet rs;
 		String sql = "select * from account where user_id = ?;";
 		psmt = con.prepareStatement(sql);
-		psmt.setString(1,user_id);
+		psmt.setString(1, user_id);
 		psmt.execute();
-		
+
 		rs = psmt.getResultSet();
 		ArrayList<accountDTO> result = new ArrayList<accountDTO>();
-		while(rs.next())
+		while (rs.next()) 
 		{
 			accountDTO temp = new accountDTO();
 			temp.setAccount_id(rs.getInt("account_id"));
@@ -38,19 +38,20 @@ public class accountDAO {
 		psmt.close();
 		return result;
 	}
-	//입출금내역 조회
-	public ArrayList<accountLogDTO> selectAccountLogs(int account_id) throws SQLException, ClassNotFoundException
+
+	// 입출금내역 조회
+	public ArrayList<accountLogDTO> selectAccountLogs(int account_id) throws SQLException, ClassNotFoundException 
 	{
 		Connection con = DBConnection.getConnection();
 		PreparedStatement psmt;
 		ResultSet rs;
 		String sql = "select * from account_log where account_id = ?;";
 		psmt = con.prepareStatement(sql);
-		psmt.setInt(1,account_id);
+		psmt.setInt(1, account_id);
 		psmt.execute();
 		rs = psmt.getResultSet();
 		ArrayList<accountLogDTO> result = new ArrayList<accountLogDTO>();
-		while(rs.next())
+		while (rs.next()) 
 		{
 			accountLogDTO temp = new accountLogDTO();
 			temp.setAccount_id(rs.getInt("account_id"));
@@ -65,93 +66,190 @@ public class accountDAO {
 		psmt.close();
 		return result;
 	}
-	//특정계좌반환
-	public accountDTO selectAccount(int account_id) throws SQLException, ClassNotFoundException
+
+	// 특정계좌반환
+	public accountDTO selectAccount(int account_id) throws SQLException, ClassNotFoundException 
 	{
 		Connection con = DBConnection.getConnection();
 		PreparedStatement psmt;
 		ResultSet rs;
 		String sql = "select * from account where account_id = ?;";
 		psmt = con.prepareStatement(sql);
-		psmt.setInt(1,account_id);
+		psmt.setInt(1, account_id);
 		psmt.execute();
 		rs = psmt.getResultSet();
-		if(!rs.next())
+		if (!rs.next())
 			return null;
 		accountDTO result;
-		result = new accountDTO(rs.getInt("account_id"), 
-				rs.getInt("type"), rs.getInt("balance"), 
+		result = new accountDTO(rs.getInt("account_id"), rs.getInt("type"), rs.getInt("balance"),
 				rs.getBoolean("is_request"), rs.getString("open_date"), rs.getString("user_id"));
 		rs.close();
 		psmt.close();
 		return result;
 	}
-	//송금(가능,불가능여부 반환)
-	public int sendMoney(int sender_account_id, int receiver_account_id,int send_amount, int receive_amount, String info) throws SQLException, ClassNotFoundException
+
+	// 송금 -1:오류 -2:존재하지 않는 계좌, -3:잔고 부족 1:성공
+	public int sendMoney(int sender_account_id, int receiver_account_id, int send_amount,
+			String info) throws SQLException, ClassNotFoundException 
 	{
 		Connection con = DBConnection.getConnection();
 		PreparedStatement psmt;
 		ResultSet rs;
-		String sql = "select balance from account where account_id = ?;";
-		psmt = con.prepareStatement(sql);
-		psmt.setInt(1,sender_account_id);
-		psmt.execute();
-		rs = psmt.getResultSet();
-		int currentBal=rs.getInt("balance");
-		psmt.close();
-		rs.close();
-		if(currentBal < send_amount)//현재 가지고있는 돈보다 더 많이 보내려고 한다면,
+		try
+		{
+			if(sender_account_id == receiver_account_id)//같은계좌 송금x
+				return -1;
+			
+			String sql = "select count(*) from account where account_id = ? or account_id = ?;";//계좌 존재여부 확인
+			psmt = con.prepareStatement(sql);
+			psmt.setInt(1, sender_account_id);
+			psmt.setInt(2, receiver_account_id);
+			psmt.execute();
+			rs = psmt.getResultSet();
+			rs.next();
+			int count = rs.getInt("count(*)");
+			if(count<2)
+			{
+				return -2;
+			}
+			
+			String sql2 = "select * from account where account_id = ?;";//송금인 계좌잔고 확인
+			psmt = con.prepareStatement(sql2);
+			psmt.setInt(1, sender_account_id);
+			psmt.execute();
+			rs = psmt.getResultSet();
+			rs.next();
+			int currentBal = rs.getInt("balance");
+			psmt.close();
+			rs.close();
+			
+			if (currentBal < send_amount)// 현재 가지고있는 돈보다 더 많이 보내려고 한다면,
+				return -3;
+			String sql3 = "update account set balance = ? where account_id = ?;";//송금인 계좌 잔고 update
+			psmt = con.prepareStatement(sql3);
+			psmt.setInt(1, currentBal - send_amount);
+			psmt.setInt(2, sender_account_id);
+			psmt.execute();
+			psmt.close();
+			
+			String sql4 = "update account set balance = balance + ? where account_id = ?;";//수금인 셰좌잔고 update
+			psmt = con.prepareStatement(sql4);
+			psmt.setInt(1, send_amount);
+			psmt.setInt(2, receiver_account_id);
+			psmt.execute();
+			psmt.close();
+			
+			String sql5 = "insert into account_log(date,amount,type,info,account_id) values(now(), ? , 0 , ? , ?);";//log 넣기
+			psmt = con.prepareStatement(sql5);
+			psmt.setInt(1, send_amount);
+			psmt.setString(2, receiver_account_id+"에게 송금");
+			psmt.setInt(3, sender_account_id);
+			psmt.addBatch();
+			
+			psmt.setInt(1, send_amount);
+			psmt.setString(2, info);
+			psmt.setInt(3, receiver_account_id);
+			psmt.addBatch();
+			
+			psmt.executeBatch();
+			psmt.close();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
 			return -1;
-		String sql2 = "update account set balance = ? where account_id = ?;";
-		psmt = con.prepareStatement(sql2);
-		psmt.setInt(1,currentBal-send_amount);
-		psmt.setInt(2,sender_account_id);
-		psmt.execute();
-		psmt.close();
-		String sql3 = "update account set balance = balance + ? where account_id = ?;";
-		psmt = con.prepareStatement(sql3);
-		psmt.setInt(1, receive_amount);
-		psmt.setInt(2, receiver_account_id);
-		psmt.execute();
-		psmt.close();
+		}
 		return 1;
 	}
-	//입금
-	public Boolean deposit(int account_id, int amount) throws SQLException, ClassNotFoundException
+
+	// 입금 -1:오류, -2:존재하지 않는 계좌, 1:성공
+	public int deposit(int account_id, int amount) throws ClassNotFoundException, SQLException 
 	{
 		Connection con = DBConnection.getConnection();
 		PreparedStatement psmt;
 		ResultSet rs;
-		String sql = "update account set balance = balance + ? where account_id = ?;";
-		psmt = con.prepareStatement(sql);
-		psmt.setInt(1, amount);
-		psmt.setInt(2, account_id);
-		boolean result = psmt.execute();
-		psmt.close();
-		return result;
-	}
-	//출금 (가능,불가능여부반환)
-	public int withdraw(int account_id, int amount) throws SQLException, ClassNotFoundException
-	{
-		Connection con = DBConnection.getConnection();
-		PreparedStatement psmt;
-		ResultSet rs;
-		String sql = "select balance from account where account_id = ?;";
-		psmt = con.prepareStatement(sql);
-		psmt.setInt(1,account_id);
-		psmt.execute();
-		rs = psmt.getResultSet();
-		int currentBal = rs.getInt("balance");
-		psmt.close();
-		rs.close();
-		if(currentBal < amount)//가지고있는 돈보다 더 많은돈을 출금하려고 할때
+		try 
+		{
+			con.setAutoCommit(false);
+			String sql2 = "select * from account where account_id = ?;";
+			psmt = con.prepareStatement(sql2);
+			psmt.setInt(1, account_id);
+			psmt.execute();
+			rs = psmt.getResultSet();
+			if (!rs.next())
+				return -2;
+			
+			String sql = "update account set balance = balance + ? where account_id = ?;";
+			psmt = con.prepareStatement(sql);
+			psmt.setInt(1, amount);
+			psmt.setInt(2, account_id);
+			psmt.execute();
+			psmt.close();
+			con.commit();
+			
+			String sql3 = "insert into account_log(date,amount,type,info,account_id) values(now(), ? , 1 , ? , ?);";//log 넣기
+			psmt = con.prepareStatement(sql3);
+			psmt.setInt(1, amount);
+			psmt.setString(2, "예금");
+			psmt.setInt(3, account_id);
+			psmt.execute();
+			
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
 			return -1;
-		String sql2 = "update account set balance = balance - ? where account_id = ?;";
-		psmt = con.prepareStatement(sql2);
-		psmt.setInt(1,amount);
-		psmt.setInt(2, account_id);
-		psmt.execute();
+		}
+		con.setAutoCommit(true);
 		return 1;
 	}
-	
+
+	// 출금 -1:오류 -2:존재하지 않는 계좌 -3:잔고 부족 1:성공
+	public int withdraw(int account_id, int amount) throws SQLException, ClassNotFoundException 
+	{
+		Connection con = DBConnection.getConnection();
+		PreparedStatement psmt;
+		ResultSet rs;
+		try 
+		{
+			String sql3 = "select * from account where account_id = ?;";
+			psmt = con.prepareStatement(sql3);
+			psmt.setInt(1, account_id);
+			psmt.execute();
+			rs = psmt.getResultSet();
+			if (!rs.next())
+				return -2;
+
+			String sql = "select * from account where account_id = ?;";
+			psmt = con.prepareStatement(sql);
+			psmt.setInt(1, account_id);
+			psmt.execute();
+			rs = psmt.getResultSet();
+			rs.next();
+			int currentBal = rs.getInt("balance");
+			psmt.close();
+			rs.close();
+			if (currentBal - amount < 0)// 가지고있는 돈보다 더 많은돈을 출금하려고 할때
+				return -3;
+			
+			String sql2 = "update account set balance = balance - ? where account_id = ?;";
+			psmt = con.prepareStatement(sql2);
+			psmt.setInt(1, amount);
+			psmt.setInt(2, account_id);
+			psmt.execute();
+			
+			String sql4 = "insert into account_log(date,amount,type,info,account_id) values(now(), ? , 0 , ? , ?);";//log 넣기
+			psmt = con.prepareStatement(sql4);
+			psmt.setInt(1, amount);
+			psmt.setString(2, "출금");
+			psmt.setInt(3, account_id);
+			psmt.execute();
+			
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return -1;
+		}
+		return 1;
+	}
+
 }
