c287be3e191e6cab6d742fd892914f0f415ce60d
kimmingyu <kimmin36944@gmail.com>
2021-11-27 02:04:19 +0900

    서블릿 수정

--- src/servlet/trans.java
+++ src/servlet/trans.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
 import dao.cardDAO;
 /**
  * Servlet implementation class trans
@@ -38,11 +40,12 @@ public class trans extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		request.setCharacterEncoding("euc-kr");
 		int result=0;
 		cardDAO cdao = new cardDAO();
 		String id = request.getParameter("card_id");
 		String amount = request.getParameter("amount");
-		String info = request.getParameter("info");
+		String info = request.getParameter("info");//URLEncoder.encode(, "euc-kr");
 		System.out.println(id+" "+amount+" "+info);
 		try {
 			result = cdao.useCard(Integer.parseInt(id),Integer.parseInt(amount), info);
@@ -56,21 +59,21 @@ public class trans extends HttpServlet {
 		{
 			out.print("<html>");
 			out.print("카드가 존재하지 않습니다.");
-			out.print("<br> <a href=\"http://localhost:8090/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
+			out.print("<br> <a href=\"/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
 			out.println("</html>");
 		}
 		else if(result==-2)
 		{
 			out.print("<html>");
 			out.print("잔고부족입니다.");
-			out.print("<br> <a href=\"http://localhost:8090/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
+			out.print("<br> <a href=\"/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
 			out.println("</html>");
 		}
 		else if(result==-3)
 		{
 			out.print("<html>");
 			out.print("한도 초과입니다.");
-			out.print("<br> <a href=\"http://localhost:8090/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
+			out.print("<br> <a href=\"/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
 			out.println("</html>");
 		}
 		//오류:-1, 잔고부족:-2, 한도초과:-3 성공:1
@@ -79,7 +82,7 @@ public class trans extends HttpServlet {
 			out.print("<html>");
 			out.print("<body>");
 			out.print(id+"번 카드에서 "+amount+"원을 사용하였습니다.");
-			out.print("<br> <a href=\"http://localhost:8090/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
+			out.print("<br> <a href=\"/DatabaseDesign/client/cardUsage.jsp\">뒤로가기</a>");
 			out.print("</body>");
 			out.println("</html>");
 		}
